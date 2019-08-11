413f829eef2f4821895719a34147b260fcf40653
developerern <developerern@gmail.com>
2019-08-11 15:30:31 +0800

    video continues playing when screen is rotated. navigation bar at the bottom appears

--- app/src/main/java/com/udacity/bakingapp/fragments/MediaPlayerFragment.java
+++ app/src/main/java/com/udacity/bakingapp/fragments/MediaPlayerFragment.java
@@ -34,8 +34,6 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 import com.udacity.bakingapp.R;
 
-import butterknife.BindView;
-
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
@@ -60,7 +58,7 @@ public class MediaPlayerFragment extends Fragment implements Player.EventListene
     // TODO: Rename and change types of parameters
     private String mParam1;
 
-//    private OnFragmentInteractionListener mListener;
+    //    private OnFragmentInteractionListener mListener;
     private int mCurrentWindow;
     private long mPlaybackPosition;
 
@@ -79,7 +77,7 @@ public class MediaPlayerFragment extends Fragment implements Player.EventListene
     @Override
     public void onResume() {
         super.onResume();
-        hideSystemUi();
+//        showSystemUi();
         if ((Util.SDK_INT <= 23 || mPlayer == null)) {
             initializePlayer();
         }
@@ -113,13 +111,14 @@ public class MediaPlayerFragment extends Fragment implements Player.EventListene
         if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
             hideSystemUiFullScreen();
         } else {
-            hideSystemUi();
+            showSystemUi();
         }
     }
 
     @SuppressLint("InlinedApi")
     private void hideSystemUiFullScreen() {
-        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        mPlayerView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -128,12 +127,11 @@ public class MediaPlayerFragment extends Fragment implements Player.EventListene
     }
 
     @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    private void showSystemUi() {
+        mPlayerView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
     /**
@@ -277,13 +275,13 @@ public class MediaPlayerFragment extends Fragment implements Player.EventListene
 //        // TODO: Update argument type and name
 //        void onFragmentInteraction(Uri uri);
 //    }
-
     private void initializePlayer() {
         if (mPlayer == null) {
             mPlayer = ExoPlayerFactory.newSimpleInstance(
                     new DefaultRenderersFactory(getContext()),
                     new DefaultTrackSelector(),
                     new DefaultLoadControl());
+            mPlayerView.requestFocus();
             mPlayerView.setPlayer(mPlayer);
             mPlayer.setPlayWhenReady(mPlayWhenReady);
             mPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
