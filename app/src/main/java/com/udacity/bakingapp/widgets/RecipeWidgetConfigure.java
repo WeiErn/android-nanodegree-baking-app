f7eb94e493d19d3e8fed9dfd70473e3aa2bedcaa
developerern <developerern@gmail.com>
2019-08-15 06:09:11 +0800

    attempt at retrieving recipes from database in widget configure activity

---
+++ app/src/main/java/com/udacity/bakingapp/widgets/RecipeWidgetConfigure.java
@@ -0,0 +1,117 @@
+package com.udacity.bakingapp.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.udacity.bakingapp.R;
+import com.udacity.bakingapp.data.Recipe;
+import com.udacity.bakingapp.database.AppDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeWidgetConfigure extends AppCompatActivity {
+    static final String TAG = "RecipeWidgetConfigure";
+
+    private static final String PREFS_NAME = "com.udacity.bakingapp.widgets.RecipeWidgetProvider";
+    private static final String PREF_PREFIX_KEY = "prefix_";
+
+    int mRecipeWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    TextView mRecipeWidgetPrefix;
+
+    public RecipeWidgetConfigure() {
+        super();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the result to CANCELED.  This will cause the widget host to cancel
+        // out of the widget placement if they press the back button.
+        setResult(RESULT_CANCELED);
+
+        // Set the view layout resource to use.
+        setContentView(R.layout.recipe_widget_configure);
+
+        // Load all recipes
+        loadAllRecipes();
+
+        // Find the EditText
+        mRecipeWidgetPrefix = findViewById(R.id.recipe_widget_prefix);
+
+        // Bind the action for the save button.
+        findViewById(R.id.save_button).setOnClickListener(mOnClickListener);
+
+        // Find the widget id from the intent.
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mRecipeWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        if (mRecipeWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+        }
+
+        mRecipeWidgetPrefix.setText(loadTitlePref(RecipeWidgetConfigure.this, mRecipeWidgetId));
+    }
+
+    private void loadAllRecipes() {
+        AppDatabase mDb = AppDatabase.getInstance(getApplicationContext());
+        List<Recipe> recipes = mDb.recipeListDao().loadAllRecipes().getValue();
+
+    }
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            final Context context = RecipeWidgetConfigure.this;
+
+            // When the button is clicked, save the string in our prefs and return that they
+            // clicked OK.
+            String titlePrefix = mRecipeWidgetPrefix.getText().toString();
+            saveTitlePref(context, mRecipeWidgetId, titlePrefix);
+
+            // Push widget update to surface with newly set prefix
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            RecipeWidgetProvider.updateAppWidget(context, appWidgetManager,
+                    mRecipeWidgetId, titlePrefix);
+
+            // Make sure we pass back the original appWidgetId
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mRecipeWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    };
+
+    // Write the prefix to the SharedPreferences object for this widget
+    static void saveTitlePref(Context context, int appWidgetId, String text) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
+        prefs.commit();
+    }
+    // Read the prefix from the SharedPreferences object for this widget.
+    // If there is no preference saved, get the default from a resource
+    static String loadTitlePref(Context context, int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        String prefix = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null);
+        if (prefix != null) {
+            return prefix;
+        } else {
+            return context.getString(R.string.appwidget_prefix_default);
+        }
+    }
+    static void deleteTitlePref(Context context, int appWidgetId) {
+    }
+    static void loadAllTitlePrefs(Context context, ArrayList<Integer> appWidgetIds,
+                                  ArrayList<String> texts) {
+    }
+}
--- app/src/main/java/com/udacity/bakingapp/widgets/RecipeWidgetProvider.java
+++ app/src/main/java/com/udacity/bakingapp/widgets/RecipeWidgetProvider.java
@@ -7,6 +7,8 @@ import android.arch.lifecycle.LiveData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.udacity.bakingapp.MainActivity;
@@ -16,6 +18,8 @@ import com.udacity.bakingapp.database.AppDatabase;
 
 import java.util.List;
 
+import static android.support.constraint.Constraints.TAG;
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -24,25 +28,32 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     private AppDatabase mDb;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                int appWidgetId, String titlePrefix) {
+        Log.d(TAG, "updateAppWidget appWidgetId=" + appWidgetId + " titlePrefix=" + titlePrefix);
+        // Getting the string this way allows the string to be localized.  The format
+        // string is filled in using java.util.Formatter-style format strings.
+        CharSequence text = context.getString(R.string.appwidget_text_format,
+                RecipeWidgetConfigure.loadTitlePref(context, appWidgetId),
+                "0x" + Long.toHexString(SystemClock.elapsedRealtime()));
+
         // Get the count from prefs.
-        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.shared_prefs_file), 0);
-        int count = sharedPreferences.getInt(context.getString(R.string.count_widget_key) + appWidgetId, 0);
-        count++;
+//        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.shared_prefs_file), 0);
+//        int count = sharedPreferences.getInt(context.getString(R.string.count_widget_key) + appWidgetId, 0);
+//        count++;
 
         // Create an Intent to launch MainActivity when clicked
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text, text);
 
         // Save count back to prefs.
-        SharedPreferences.Editor prefEditor = sharedPreferences.edit();
-        prefEditor.putInt(context.getString(R.string.count_widget_key) + appWidgetId, count);
-        prefEditor.apply();
+//        SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+//        prefEditor.putInt(context.getString(R.string.count_widget_key) + appWidgetId, count);
+//        prefEditor.apply();
 
         // Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
@@ -53,12 +64,18 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        mDb = AppDatabase.getInstance(context);
-        List<Recipe> recipes = mDb.recipeListDao().loadAllRecipes().getValue();
+//        mDb = AppDatabase.getInstance(context);
+//        List<Recipe> recipes = mDb.recipeListDao().loadAllRecipes().getValue();
         
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+//        for (int appWidgetId : appWidgetIds) {
+//            updateAppWidget(context, appWidgetManager, appWidgetId);
+//        }
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+            String titlePrefix = RecipeWidgetConfigure.loadTitlePref(context, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId, titlePrefix);
         }
     }
 
